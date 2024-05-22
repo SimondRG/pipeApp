1ddb7cc076f7557648c2c8a1affac691f548e10c
Simon Rojas <sdavid.rojas@hotmail.com>
2024-05-22 15:04:04 -0500

    Elaboración del componente numbers-page para el manejo de los Pipes number, currency y percent, así como el desarrollo del componente uncommon-page para el manejo de los Pipes i18nSelect, i18nPlural y Slice

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Simón';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'initarla'
+  }
+
+  changeCliente(): void {
+    if (this.name === 'Simón') {
+      this.name = 'Lizeth';
+      this.gender = 'female'
+    } 
+    else{
+      this.name = 'Simón';
+      this.gender = 'male'
+    }
+  }
+
+  // i18nPlural
+  public clientes: string[] = ['Blanca', 'Simon', 'Lizeth', 'David', 'Leonardo', 'Laura', 'Valentina'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 esperando.',
+    'other': 'tenemos # clientes esperando.',
+  }
+
+  deleteClient(): void{
+    this.clientes.shift();
+  }
+
+
 }
